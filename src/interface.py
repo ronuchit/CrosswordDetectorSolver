c5b5183bda50a4d333a224beaba18c8f0aa8f11d
Rohan <ronuchit@yahoo.com>
2013-10-18 22:18:39 -0700

    prelim interface  testing continued

--- src/interface.py
+++ src/interface.py
@@ -5,8 +5,8 @@ class Foo(object):
   def __init__(self):
     self.obj = lib.Test_new()
   def bar(self):
-    lib.Test_bar(self.obj, 5)
+    return lib.Test_bar(self.obj, 5)
 
 if __name__ == "__main__":
   f = Foo()
-  f.bar()
+  print(f.bar())
--- src/test.cpp
+++ src/test.cpp
@@ -2,16 +2,21 @@
 
 class Test {
     public:
-        void bar(int x) {
-            std::cout << x << std::endl;
+        int* bar(int x) {
+          int* a;
+          int b[] = {1, 2};
+          a = b;
+          return a;
         }
 };
 
 extern "C" {
-    Test* Test_new() {
-      return new Test();
-    }
-  void Test_bar(Test* test, int x) {
-      test->bar(x);
-    }
+  Test* Test_new() {
+    return new Test();
+  }
+  int* Test_bar(Test* test, int x) {
+    int* a = test->bar(x);
+    std::cout << a[0] << "\n";
+    std::cout << a[1] << "\n";
+  }
 }
