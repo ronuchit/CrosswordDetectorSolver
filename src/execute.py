195e12830a3444fa895c93544fddda985c9d3bd1
Rohan <ronuchit@yahoo.com>
2013-10-19 03:23:00 -0700

    adding more hard coded clues

--- src/execute.py
+++ src/execute.py
@@ -14,11 +14,15 @@ class Executor(object):
     self.b = board.Board(self.color_array)
 
   def execute(self):
-    c1 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0, 0), "whe")
-    c1.word_list = ["heys"]
-    c2 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(7, 4), "whe2")
+    c1 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0, 0), "c1")
+    c1.word_list = ["hiya"]
+    c2 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(7, 4), "c2")
     c2.word_list = ["listen"]
-    solver.Solver(self.b.board, [c1, c2]).solve()
+    c3 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(4, 8), "c3")
+    c3.word_list = ["forever"]
+    c4 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(12, 11), "c4")
+    c4.word_list = ["baz"]
+    solver.Solver(self.b.board, [c1, c2, c3, c4]).solve()
     self.produce_output()
 
   def create_color_array(self):
@@ -36,7 +40,10 @@ class Executor(object):
     with open("../temp/solution_info.txt", "w+") as f:
       for row in range(side_length):
         for column in range(side_length):
-          f.write("%s %s %s\n"%(str(self.b.board[row, column].letter), row, column))
+          if self.b.board[row, column].color == board.Board.BLACK:
+            f.write("black %s %s\n"%(row, column))
+          else:
+            f.write("%s %s %s\n"%(str(self.b.board[row, column].letter), row, column))
   
 if __name__ == "__main__":
   Executor().execute()
