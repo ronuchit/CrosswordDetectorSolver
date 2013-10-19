384b971dfd610e25f4bb0ae7503b22cf6959971c
Rohan <ronuchit@yahoo.com>
2013-10-19 02:28:09 -0700

    only input file name and num blocks

--- src/input.sh
+++ src/input.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
 if [ $# -ne 3 ] ; then
-    echo "Usage: bash $0 [PUZZLE_SIZE] [BLOCK_SIZE] [IMAGE_NAME]"
+    echo "Usage: bash $0 [IMAGE_FILE_NAME] [NUM_BLOCKS]"
     exit
 fi
 
 mkdir ../temp
 echo $1 > ../temp/puzzle_info.txt
 echo $2 >> ../temp/puzzle_info.txt
-echo $3 >> ../temp/puzzle_info.txt
 
 # call C++ puzzle processor
 make
