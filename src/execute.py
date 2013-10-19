03aef933c7d9cf2aae72b41e3b8f68ee413d4438
Sydney <sydneymwong@gmail.com>
2013-10-19 03:34:08 -0700

    commiting execute

--- src/execute.py
+++ src/execute.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 import board
-# import scraper
+import scraper
 import solver
 import clue
 import coordinate
@@ -14,16 +14,25 @@ class Executor(object):
     self.b = board.Board(self.color_array)
 
   def execute(self):
-    c1 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0, 0), "c1")
-    c1.word_list = ["hiya"]
-    c2 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(7, 4), "c2")
-    c2.word_list = ["listen"]
-    c3 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(4, 8), "c3")
-    c3.word_list = ["forever"]
-    c4 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(12, 11), "c4")
-    c4.word_list = ["baz"]
-    solver.Solver(self.b.board, [c1, c2, c3, c4]).solve()
-    self.produce_output()
+    clue1 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(3,0), "Ump's call")
+    clue4 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0,0), "Branch")
+    clue8 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(6,12), "From __ Z (2 wds.)")
+    clue11 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(8,0), '"__ was saying..." (2 wds.)')
+    clue12 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0,5), "Teens' heroes")
+    clue14 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(11,12), "Candidate, for short")
+    # clue15 = Clue(Board.ACROSS, Coordinate(), "Opposite of masc.")
+    # clue16 = Clue(Board.ACROSS, Coordinate(), "Arranged in rows and columns")
+    # clue18 = Clue(Board.ACROSS, Coordinate(), "Promissory Note")
+
+    # clue1d = Clue(Board.DOWN, Coordinate(), "Lout")
+    # clue2d = Clue(Board.DOWN, Coordinate(), "Application")
+    # clue3d = Clue(Board.DOWN, Coordinate(), "Shyness")
+    # clue4d = Clue(Board.DOWN, Coordinate(), "Illuminated (2 wds.)")
+    # clue5d = Clue(Board.DOWN, Coordinate(), "Boise's state (abbr.)")
+
+    scraper.Scraper().run_scraper([clue1, clue2, clue3, clue4], self.b.board)
+    #solver.Solver(self.b.board, [c1, c2]).solve()
+    #self.produce_output()
 
   def create_color_array(self):
     with open("../temp/color_info.txt", "r+") as f:
