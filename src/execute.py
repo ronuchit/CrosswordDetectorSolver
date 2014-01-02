cd964c11354ca674475da2f1f4019c843b41f00e
Rohan <ronuchit@yahoo.com>
2014-01-01 21:07:39 -0800

    fixed bug so clues no longer must be in specific order, cleaned up execute.py

--- src/execute.py
+++ src/execute.py
@@ -15,25 +15,11 @@ class Executor(object):
     self.b = board.Board(self.color_array)
 
   def execute(self):
-    # clue1 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(0,10), "Hawaiian souvenir")
-    # clue2 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(0,11), "Chicago trains")
-    # clue3 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(1,9), "Soccer legend")
-    # clue4 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(2,8), "Circle segment")
-    # clue5 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(1,9), "Implore")
-    # clue6 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(3,7), "Mine find")
-    # clue7 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(4, 8), '"Cheers" waitress')
-    # clue8 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(5, 9), "Regard")
-    # clue9 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(4,11), "Renting contract")
-    # clue10 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(0,7), "Toast topping")
-    # clue11 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(4,12), "Stockpile")
-    # clue12 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(7,5), "Simplicity")
-    # clue13 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(8,6), "Settings")
-    # clues = [clue3, clue1, clue2, clue4, clue5, clue6, clue7, clue8, clue9, clue10, clue11, clue12, clue13]
     clues = clue_parser.parse(self.b)
-    clues = [clues[0], clues[7], clues[8], clues[10], clues[9], clues[1], clues[2], clues[3], clues[11], clues[6], clues[12], clues[4], clues[5]]
     
     scraper.Scraper().run_scraper(clues, self.b.board)
-    solver.Solver(self.b.board, clues).solve()
+    print("Solving puzzle...")
+    solver.Solver(self.b, clues).solve()
     self.produce_output()
 
   def create_color_array(self):
--- src/solver.py
+++ src/solver.py
@@ -2,7 +2,7 @@
 
 import board
 import numpy as np
-import sys
+import copy
 
 class IncompatibleLengthException(Exception):
   pass
@@ -11,8 +11,8 @@ class SolverFailedError(Exception):
   pass
 
 class Solver(object):
-  def __init__(self, board_repr, clues):
-    self.board_repr = board_repr
+  def __init__(self, b, clues):
+    self.board_instance = b
     self.clues = clues
     self.longer_clues = []
     self.num_remaining = len(self.clues)
@@ -27,25 +27,27 @@ class Solver(object):
   def solve(self):
     self.fill_guarantees()
     if len(self.longer_clues) > 0:
-      self._solve()
+      self._solve(self.board_instance.board)
 
-  def _solve(self, words=[]):
+  def _solve(self, curr_board):
     if len(self.longer_clues) > 0:
       clue = self.longer_clues[self.index]
       for word in clue.word_list:
+        new_board = copy.deepcopy(curr_board)
         try:
-          if self.place_letters(clue.coord.x, clue.coord.y, word, clue.direction):
+          if self.place_letters(clue.coord.x, clue.coord.y, word, clue.direction, new_board): # complete solution found
+            self.board_instance.board = new_board
             return True
         except board.InvalidLetterException:
           continue
         self.index += 1
         try:
-          if self._solve(words+[word]):
+          if self._solve(new_board):
             return True
         except SolverFailedError:
           pass
         self.index -= 1
-        self.unplace_letters(clue.coord.x, clue.coord.y, word, clue.direction, words)
+        self.num_remaining += 1
     raise SolverFailedError("No solution found!")
 
   def fill_guarantees(self):
@@ -54,31 +56,29 @@ class Solver(object):
         word = clue.word_list[0]
         c_x, c_y = clue.coord.x, clue.coord.y
         if clue.direction == board.Board.ACROSS:
-          length = self.board_repr[c_x, c_y].right_length
+          length = self.board_instance.board[c_x, c_y].right_length
         else:
-          length = self.board_repr[c_x, c_y].down_length
+          length = self.board_instance.board[c_x, c_y].down_length
           
-        self.check_length_validity(length, word)
         try:
-          if self.place_letters(c_x, c_y, word, clue.direction):
-            return
+          self.place_letters(c_x, c_y, word, clue.direction, self.board_instance.board)
         except board.InvalidLetterException:
           raise SolverFailedError("Found incompatible intersection when solving guarantees (this shouldn't happen)!")
 
-  def place_letters(self, c_x, c_y, word, direction):
+  def place_letters(self, c_x, c_y, word, direction, curr_board):
     x, y = c_x, c_y
     old_letters = []
     for i in range(len(word)):
       try:
-        old_letters.append(self.board_repr[x, y].letter)
-        self.board_repr[x, y].set_letter(word[i])
+        old_letters.append(curr_board[x, y].letter)
+        curr_board[x, y].set_letter(word[i])
       except board.InvalidLetterException as e:
         if direction == board.Board.DOWN:
           for corr_x in range(c_x, x):
-            self.board_repr[corr_x, y].set_letter(old_letters[corr_x - c_x])
+            curr_board[corr_x, y].set_letter(old_letters[corr_x - c_x])
         else:
           for corr_y in range(c_y, y):
-            self.board_repr[x, corr_y].set_letter(old_letters[corr_y - c_y])
+            curr_board[x, corr_y].set_letter(old_letters[corr_y - c_y])
         raise e
       if direction == board.Board.DOWN:
         x += 1
@@ -90,31 +90,16 @@ class Solver(object):
       return True
     return False
 
-  def unplace_letters(self, c_x, c_y, word, direction, words):
-    x, y = c_x, c_y
-    for i in range(len(word)):
-      if direction == board.Board.DOWN:
-        if (y == 0 or self.board_repr[x, y-1].letter is None) and (y == np.shape(self.board_repr)[0] - 1 or self.board_repr[x, y+1].letter is None):
-          self.board_repr[x, y].set_letter(None)
-        x += 1
-      else:
-        if (x == 0 or self.board_repr[x-1, y].letter is None) and (x == np.shape(self.board_repr)[0] - 1 or self.board_repr[x+1, y].letter is None):
-          self.board_repr[x, y].set_letter(None)
-        y += 1
-    self.num_remaining += 1
-
-  def check_length_validity(self, req_len, word):
-    if req_len != len(word):
-      raise IncompatibleLengthException("%s is not of length %d"%(word, req_len))
-
 if __name__ == "__main__":
   import clue, coordinate
   color_arr = np.array([[1, 1, 1, 0, 0], [1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 1], [0, 0, 0, 1, 1]])
   b = board.Board(color_arr)
   b.construct_board()
-  c1 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0, 0), "whe")
-  c1.word_list = ["hey"]
+  c1 = clue.Clue(board.Board.ACROSS, coordinate.Coordinate(0, 0), "whe1")
+  c1.word_list = ["hey", "wey"]
   c2 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(0, 0), "whe2")
-  c2.word_list = ["gist"]
-  Solver(b.board, [c1, c2]).solve()
+  c2.word_list = ["gist", "hist"]
+  c3 = clue.Clue(board.Board.DOWN, coordinate.Coordinate(0, 1), "whe3")
+  c3.word_list = ["fein", "eins"]
+  Solver(b, [c3, c2, c1]).solve()
   b.print_board()
